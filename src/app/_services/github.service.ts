2671db1774192b236112eb248fa98bbd4c3c52d2
Christoph Krieger <krieger.christoph1@gmail.com>
2018-01-18 11:50:05 +0100

    docs

--- src/app/_services/github.service.ts
+++ src/app/_services/github.service.ts
@@ -28,7 +28,7 @@ export class GithubService {
   private getUserName() {
     console.log((this.platformLocation as any).location);
     console.log((this.platformLocation as any).location.href);
-    const baseUrl = (this.platformLocation as any).location.origin;
+    const baseUrl = (this.platformLocation as any).location;
     console.log(baseUrl);
     const userNameReg = /\https:\/\/+(.*)(?=.github.io)/g;
     const username = userNameReg.exec(baseUrl);
@@ -36,7 +36,7 @@ export class GithubService {
   }
 
   private getRepoName() {
-    const baseUrl = (this.platformLocation as any).location.origin;
+    const baseUrl = (this.platformLocation as any).location;
     const repoReg = /\github.io\/+(.*)(?=\/)/g;
     const repoName = repoReg.exec(baseUrl);
     return repoName[1];
