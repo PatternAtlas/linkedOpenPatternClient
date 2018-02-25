65ca40bb84afb91607b1e8bbf0d7fc076693f5ca
Christoph Krieger <krieger.christoph1@gmail.com>
2018-02-25 18:15:57 +0100

    display all classes of a given vocab in a treeview

--- src/app/ontology-browser/browser-classes-overview/browser-classes-overview.component.ts
+++ src/app/ontology-browser/browser-classes-overview/browser-classes-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TreeComponent, TreeNode } from 'angular-tree-component';
 
 @Component({
   selector: 'app-browser-classes-overview',
@@ -7,34 +8,118 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowserClassesOverviewComponent implements OnInit {
 
-  nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      children: [
-        { id: 5, name: 'child2.1' },
-        {
-          id: 6,
-          name: 'child2.2',
-          children: [
-            { id: 7, name: 'subsub' }
-          ]
-        }
-      ]
+  nodes = [];
+  selectedNode = {
+    label: '',
+    terms: '',
+    relations: [],
+    disjointWith: [],
+    instances: []
+  };
+  options = {
+    getChildren: (node: TreeNode) => {
+      return this.getChildren(node.data);
     }
-  ];
-  options = {};
+  };
+
+  @ViewChild(TreeComponent)
+  private tree: TreeComponent;
+
   constructor() { }
 
   ngOnInit() {
+    this.loadClasses();
+  }
+
+  loadClasses() {
+    jOWL.load('assets/wine.rdf', () => {
+      new jOWL.SPARQL_DL('Class(?x)').execute({
+        onComplete: (result) => {
+          this.onJowlComplete(result);
+        }
+      });
+    });
+  }
+
+  onJowlComplete(results) {
+    const rootParents = new Set();
+    const classesOfVocab = results.jOWLArray('?x').items;
+    classesOfVocab.forEach(element => {
+      const itemArray = element.hierarchy(true);
+      itemArray.each((el) => {
+        if (el.invParents) {
+          rootParents.add(el);
+        }
+      });
+    });
+    this.addNodesToTreeView(rootParents);
+  }
+
+  addNodesToTreeView(nodes) {
+    nodes.forEach(el => {
+      this.nodes.push({
+        id: this.nodes.length + 1,
+        name: el.name,
+        hasChildren: true,
+        jowlNode: el
+      });
+    });
+    this.tree.treeModel.update();
+  }
+
+  getChildren(parentNode) {
+    const children = [];
+    parentNode.jowlNode.invParents.items.forEach(child => {
+      children.push({
+        name: child.name,
+        hasChildren: typeof (child.invParents) !== 'undefined',
+        jowlNode: child
+      });
+    });
+    return children;
+  }
+
+
+  nodeClickEvent(node) {
+    new jOWL.SPARQL_DL(`PropertyValue(${node.label()}, ?p, ?t)`).execute({
+      onComplete: (result) => {
+        this.selectedNode.label = node.label();
+        this.selectedNode.terms = this.getTerms(node);
+        this.selectedNode.relations = result.jOWLArray('?p', '?t').items;
+        this.selectedNode.disjointWith = this.getDisjointWith(node);
+        this.getInstanceOfClass(node);
+      }
+    });
+  }
+
+  getTerms(node) {
+    let terms = '';
+    node.terms().forEach(element => {
+      terms += `${element[0]} `;
+    });
+    return terms;
+  }
+
+  getDisjointWith(item) {
+    if (!(item.isClass)) { return; }
+    return $.map(
+      jOWL.Xpath('*', item.jnode)
+        .filter(function () { return this.nodeName === 'owl:disjointWith'; }),
+      function (n, i) {
+        return { 'owl:disjointWith': jOWL($(n).RDF_Resource()) };
+      });
+  }
+
+  getInstanceOfClass(owlClass) {
+    new jOWL.SPARQL_DL(`DirectType(?i, ${owlClass.label()})`).execute({
+      onComplete: (result) => {
+        this.selectedNode.instances = result.jOWLArray('?i').items;
+      }
+    });
   }
 
 }
+
+
+
+
