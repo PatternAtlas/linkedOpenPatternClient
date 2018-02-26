c6d3336901a7e2c61de5b9e6113c64f4e707e5fe
Christoph Krieger <krieger.christoph1@gmail.com>
2018-02-26 12:02:02 +0100

    add tab that shows all individuals

--- src/app/ontology-browser/individuals-overview/individuals-overview.component.ts
+++ src/app/ontology-browser/individuals-overview/individuals-overview.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndividualsOverviewComponent implements OnInit {
 
+  individuals = [];
+  selectedIndividual;
   constructor() { }
 
   ngOnInit() {
+    this.getIndividuals();
+  }
+
+  getIndividuals() {
+    jOWL.load('assets/wine.rdf', () => {
+      const arr = new jOWL.Ontology.Array();
+      // tslint:disable-next-line:forin
+      for (const key in jOWL.index('Thing')) {
+        this.individuals.push(jOWL.index('Thing')[key]);
+        arr.concat(jOWL.index('Thing')[key], true);
+      }
+      this.individuals = arr.items;
+    });
+  }
+
+  onIndividualSelected(individual) {
+    new jOWL.SPARQL_DL(`PropertyValue(${individual.label()}, ?p, ?t)`).execute({
+      onComplete: (result) => {
+        this.selectedIndividual = individual;
+        this.selectedIndividual.propertyValues = result.results;
+      }
+    });
   }
 
 }
--- src/app/ontology-browser/properties-overview/properties-overview.component.ts
+++ src/app/ontology-browser/properties-overview/properties-overview.component.ts
@@ -10,6 +10,18 @@ export class PropertiesOverviewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getIndividuals();
+  }
+
+  getIndividuals() {
+    jOWL.load('assets/wine.rdf', () => {
+      const arr = new jOWL.Ontology.Array();
+      // tslint:disable-next-line:forin
+      for (const key in jOWL.index('Thing')) {
+        arr.concat(jOWL.index('Thing')[key], true);
+      }
+      console.log(arr);
+    });
   }
 
 }
